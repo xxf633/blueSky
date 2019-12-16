f767ff006372358cf23bb87306823fa2c469e7e6
xxfgjx <2093457560@qq.com>
2019-12-16 08:49:34 +0800

    这是修改后的redisDemo

--- src/main/java/com/baidu/Test1.java
+++ src/main/java/com/baidu/Test1.java
@@ -4,4 +4,13 @@ package com.baidu;
  * Created by xxf123 on 2019/10/30.
  */
 public class Test1 {
+    private static String name;
+    private static  int age;
+    private static String addr;
+    public static void main(String[] args) {
+       String name="哇哈哈";
+        int age=19;
+        String addr="湖南长沙哦";
+        System.out.println(name+":张三"+age+":18"+addr+"武当山");
+    }
 }
